3bec882305a499373070751b578a763bed83a5b8
samuelchristlie <samuel.christlie@binus.ac.id>
2023-05-24 10:56:45 +0700

    Home draft

--- FanHub/resources/views/home.blade.php
+++ FanHub/resources/views/home.blade.php
@@ -13,65 +13,71 @@
         />
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-
-    <div class="navbar bg-base-100">
-        <div class="flex-1">
-            <a href="" class="btn btn-ghost"
-                ><img src="/images/icons/LogoFanHub 1.png" class="h-12" alt="Logo"
-            /></a>
-        </div>
-
-        <div class="flex-none gap-2">
-            <div class="form-control">
-                <input
-                    type="text"
-                    placeholder="Search"
-                    class="input input-bordered"
-                />
+    <body>
+        <div class="navbar bg-base-100 px-16">
+            <div class="flex-1">
+                <a href="" class="btn btn-ghost"
+                    ><img
+                        src="/images/icons/LogoFanHub 1.png"
+                        class="h-12"
+                        alt="Logo"
+                /></a>
             </div>
-            <button class="btn btn-ghost btn-circle">
-                <div class="indicator">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                        />
-                    </svg>
-                    <span
-                        class="badge badge-xs badge-primary indicator-item"
-                    ></span>
+
+            <div class="flex-none gap-2">
+                <div class="form-control">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        class="input input-bordered"
+                    />
                 </div>
-            </button>
-            <div class="dropdown dropdown-end">
-                <div class="avatar placeholder">
-                    <div
-                        class="bg-neutral-focus text-neutral-content rounded-full w-8"
-                    >
-                        <span>M</span>
+                <button class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                            />
+                        </svg>
+                        <span
+                            class="badge badge-xs badge-primary indicator-item"
+                        ></span>
                     </div>
+                </button>
+                <div class="dropdown dropdown-end">
+                    <div class="avatar placeholder">
+                        <div
+                            class="bg-neutral-focus text-neutral-content rounded-full w-8"
+                        >
+                            <span>M</span>
+                        </div>
+                    </div>
+                    <ul
+                        tabindex="0"
+                        class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                    >
+                        <li>
+                            <a class="justify-between">
+                                Profile
+                                <span class="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
                 </div>
-                <ul
-                    tabindex="0"
-                    class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-                >
-                    <li>
-                        <a class="justify-between">
-                            Profile
-                            <span class="badge">New</span>
-                        </a>
-                    </li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
             </div>
         </div>
-    </div>
+
+
+    </body>
 </html>
--- FanHub/routes/web.php
+++ FanHub/routes/web.php
@@ -16,10 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-=======
 
 Route::get('home', function () {
     return view('home');
 });
->>>>>>> 6478ee5 (Home draft)
