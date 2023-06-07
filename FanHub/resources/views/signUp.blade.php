bf738b471765f0d94868db96093ff593739ed8f7
nangzy <nicholaus@binus.ac.id>
2023-06-07 13:15:05 +0700

    signUp done (tailwind)

--- FanHub/resources/views/signUp.blade.php
+++ FanHub/resources/views/signUp.blade.php
@@ -1,42 +1,67 @@
 @extends('template')
 
 @section('content')    
-    <div class="flex justify-center text-3xl font-semibold mb-5">
+    <div class="flex justify-center text-3xl font-semibold">
         Account Sign Up
     </div>
 
     <form class="form-control font-semibold text-2xl card">
       <div class="card-body">
-        <div class="flex justify-center container">
-          <div class="mb-3">
-            <label for="inputFirstName" class="form-label">First Name</label>
-            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputFirstName">
+        <div class="flex flex-col justify-center items-center">
+          <div class="mb-2">
+            <div class="mb-2">
+              <label for="inputFirstName" class="form-label">First Name</label>
+            </div>
+            <div>
+              <input type="text" placeholder="Type here" class="input input-bordered w-96 max-w-xs" id="inputFirstName">
+            </div>
           </div>
-      </div>
-      <div class="flex justify-center container">
-          <div class="mb-3">
-            <label for="inputLastName" class="form-label">Last Name</label>
-            <input type="text"  placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputLastName">
+          
+          <div class="mb-2">
+            <div class="mb-2">
+              <label for="inputLastName" class="form-label">Last Name</label>
+            </div>
+            <div>
+              <input type="text"  placeholder="Type here" class="input input-bordered w-96 max-w-xs" id="inputLastName">
+            </div>
           </div>
-      </div>
-      <div class="flex justify-center container">
-          <div class="mb-3">
-            <label for="inputUserName" class="form-label">Username</label>
-            <input type="text"  placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputUserame">
+      
+      
+          <div class="mb-2">
+            <div class="mb-2">
+              <label for="inputUserName" class="form-label">Username</label>
+            </div>
+            <div>
+              <input type="text"  placeholder="Type here" class="input input-bordered w-96 max-w-xs" id="inputUserame">
+            </div>
           </div>
-      </div>
-      <div class="flex justify-center container">
-          <div class="mb-3">
-            <label for="inputEmail" class="form-label">Email</label>
-            <input type="email" placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputEmail">
+      
+      
+          <div class="mb-2">
+            <div class="mb-2">
+              <label for="inputEmail" class="form-label">Email</label>
+            </div>
+            <div>
+              <input type="email" placeholder="Type here" class="input input-bordered w-96 max-w-xs" id="inputEmail">
+            </div>
           </div>
-      </div>
-      <div class="flex justify-center container">
-          <div class="mb-3">
-            <label for="inputPassword" class="form-label">Password</label>
-            <input type="password"  placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputPassword">
+      
+      
+          <div class="mb-2">
+            <div class="mb-2">
+              <label for="inputPassword" class="form-label">Password</label>
+            </div>
+            <div>
+              <input type="password"  placeholder="Type here" class="input input-bordered w-96 max-w-xs" id="inputPassword">
+            </div>
           </div>
-      </div>
+          
+          <div class="mt-2">
+            <button class="btn w-80 text-[#FFFFFF]" style="background-color: #AE466F"> Sign Up</button>
+          </div>
+          </div>
+        </div>
+
       </div>
         
     </form>
