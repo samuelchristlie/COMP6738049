8da6f373a3580ba040060fe23f62e3f2c076e674
nangzy <nicholaus@binus.ac.id>
2023-06-07 10:47:43 +0700

    signUp

--- FanHub/resources/views/signUp.blade.php
+++ FanHub/resources/views/signUp.blade.php
@@ -1,44 +1,43 @@
 @extends('template')
 
 @section('content')    
-    <div class="d-flex justify-content-center fs-2 fw-semibold p-3">
+    <div class="flex justify-center text-3xl font-semibold mb-5">
         Account Sign Up
     </div>
 
-    <form class="fw-semibold">
-        <div class="d-flex flex-column justify-content-center container-fluid col-5">
-            <div class="mb-3">
-              <label for="inputFirstName" class="form-label">First Name</label>
-              <input type="text" class="form-control" id="inputFirstName">
-            </div>
-        </div>
-        <div class="d-flex flex-column justify-content-center container-fluid col-5">
-            <div class="mb-3">
-              <label for="inputLastName" class="form-label">Last Name</label>
-              <input type="text" class="form-control" id="inputLastName">
-            </div>
-        </div>
-        <div class="d-flex flex-column justify-content-center container-fluid col-5">
-            <div class="mb-3">
-              <label for="inputUserName" class="form-label">Username</label>
-              <input type="text" class="form-control" id="inputUserName">
-            </div>
-        </div>
-        <div class="d-flex flex-column justify-content-center container-fluid col-5">
-            <div class="mb-3">
-              <label for="inputEmail" class="form-label">Email</label>
-              <input type="Email" class="form-control" id="inputEmail">
-            </div>
-        </div>
-        <div class="d-flex flex-column justify-content-center container-fluid col-5">
-            <div class="mb-3">
-              <label for="inputPassword" class="form-label">Password</label>
-              <input type="password" class="form-control" id="inputPassword">
-            </div>
-        </div>
+    <form class="form-control font-semibold text-2xl card">
+      <div class="card-body">
+        <div class="flex justify-center container">
+          <div class="mb-3">
+            <label for="inputFirstName" class="form-label">First Name</label>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputFirstName">
+          </div>
+      </div>
+      <div class="flex justify-center container">
+          <div class="mb-3">
+            <label for="inputLastName" class="form-label">Last Name</label>
+            <input type="text"  placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputLastName">
+          </div>
+      </div>
+      <div class="flex justify-center container">
+          <div class="mb-3">
+            <label for="inputUserName" class="form-label">Username</label>
+            <input type="text"  placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputUserame">
+          </div>
+      </div>
+      <div class="flex justify-center container">
+          <div class="mb-3">
+            <label for="inputEmail" class="form-label">Email</label>
+            <input type="email" placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputEmail">
+          </div>
+      </div>
+      <div class="flex justify-center container">
+          <div class="mb-3">
+            <label for="inputPassword" class="form-label">Password</label>
+            <input type="password"  placeholder="Type here" class="input input-bordered w-full max-w-xs" id="inputPassword">
+          </div>
+      </div>
+      </div>
+        
     </form>
-
-    <div class="d-flex justify-content-center">
-        <button class="btn text-light" type="button"style="--bs-btn-color: black;">Sign Up</button>
-    </div>
 @endsection
